b913598936d1577317c68b9df158fcb1613368d3
oybekalloyorov <alloyorovoybek1997@gmail.com>
2025-09-19 15:06:17 +0500

    Go darslari

--- main.go
+++ main.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Println("Hello, world!")
+	// fmt.Println("Hello, world!")
+	// var x, y int = 3, 5
+	// var y int = 5
+
+	// var sum int = x + y
+	// fmt.Println(sum)
+	//ozgaruvchida qisqa ko'rinishda foydalanish
+	x, y := 3, 5
+	// y := 5
+	sum := x + y
+	fmt.Println(sum)
 }
