701ddb78a57f8a0d4fe79e0f3a3342404ac65168
oybekalloyorov <alloyorovoybek1997@gmail.com>
2025-09-20 16:48:19 +0500

    chanel dan foydalanish va random tarzda sonlarni chiqarish

--- oybek-go-darslari/12-dars/main.go
+++ oybek-go-darslari/12-dars/main.go
@@ -3,54 +3,59 @@
 package main
 
 import (
+	"math/rand"
 	"fmt"
 	"time"
 )
 
+// func main() {
+// 	channel1 := make(chan string)
+// 	channel2 := make(chan string)
+
+// 	go func() {
+// 		for {
+// 			channel1 <- "Tez"
+// 			time.Sleep(time.Millisecond * 100)
+// 		}
+// 	}()
+
+// 	go func() {
+// 		for {
+// 			channel2 <- "Sekin"
+// 			time.Sleep(time.Second * 2)
+// 		}
+// 	}()
+
+// 	for {
+// 		fmt.Println(<-channel1)
+// 		fmt.Println(<-channel2)
+// 	}
+// }
+
+// 2-misol
 func main() {
-	channel1 := make(chan string)
-	channel2 := make(chan string)
+	var n int
+	fmt.Print("Nechta tasodifiy son chiqarilsin? ")
+	fmt.Scan(&n)
 
-	go func() {
-		for {
-			channel1 <- "Tez"
-			time.Sleep(time.Millisecond * 100)
-		}
-	}()
-
-	go func() {
-		for {
-			channel2 <- "Sekin"
-			time.Sleep(time.Second * 2)
-		}
-	}()
-
-	for {
-		fmt.Println(<-channel1)
-		fmt.Println(<-channel2)
-	}
-}
-
-/* // 2-misol
-func main() {
 	channel := make(chan int)
-	go getRandomNumber(channel)
+	go getRandomNumber(channel, n)
 	for randomNumber := range channel {
 		fmt.Println("tasodifiy son: ", randomNumber)
 	}
 
 }
 
-func getRandomNumber(channel chan int) {
+func getRandomNumber(channel chan int, count int) {
 	rand.Seed(time.Now().UnixNano())
-	for i := 1; i <= 3; i++ {
+	for i := 1; i <= count; i++ {
 		number := rand.Intn(1000)
-		time.Sleep(time.Second * 1)
+		// time.Sleep(time.Second * 1)
 		channel <- number
 	}
 	close(channel)
 }
-*/
+
 
 /* // 1-misol
 func main() {
