2a825e8952d656b21e2b1239fbd95110a0ad13c4
oybekalloyorov <alloyorovoybek1997@gmail.com>
2025-09-20 14:05:18 +0500

    go darslari

---
+++ oybek-go-darslari/8-dars/main.go
@@ -0,0 +1,23 @@
+// struct haqida
+
+package main
+
+import "fmt"
+
+type car struct {
+	Make, Model, Color string
+	Year, Weight       int
+	Engine             engine
+	Oybek			   engine
+}
+
+type engine struct {
+	name string
+	hp   int
+}
+
+func main() {
+	//var myCar Car
+	myCar := car{Make: "Volvo", Model: "XC90", Color: "White", Year: 2020, Weight: 2343, Engine: engine{"T8", 400}, Oybek: engine{"Oybek", 28}}
+	fmt.Println(myCar)
+}
