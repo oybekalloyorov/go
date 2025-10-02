81d4513dab9bc561383d53c8d62e1861ee2b9342
oybekalloyorov <alloyorovoybek1997@gmail.com>
2025-10-02 17:02:00 +0500

    Map dan foydalanish

---
+++ map2.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var mp map[int]string = map[int]string{}
+	// mp[5] = "Well"
+	// // fmt.Println(mp)
+	// mp[5] = "Good"
+	// fmt.Println(mp)
+	// mp[6] = "Best"
+	// fmt.Println(mp)
+
+	// 2 yo'l
+	// mp := map[int]string{}
+	// mp[28] = "Well"
+	// fmt.Println(mp)
+
+	// 3 yol 
+	mp := make(map[int]string)
+	mp[28] = "Well"
+	fmt.Println(mp)
+}
