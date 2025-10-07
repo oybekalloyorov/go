b73c5bc229078ac353af5b3f33ea1adda7e0eb6f
oybekalloyorov <alloyorovoybek1997@gmail.com>
2025-10-07 17:23:15 +0500

    Generics

--- test2.go
+++ test2.go
@@ -9,9 +9,11 @@ func printItem[T any](item, defaultValue T)(T, T) {
 
 func main() {
 	num1, num2 := printItem(10, 20)
-	str1, str2 := printItem("Hello", "World")
+	str1, str2 := printItem[string]("Hello", "World")
+	bool1, bool2 := printItem[bool](true, false)
 
 	fmt.Println(num1, num2)
 	fmt.Println(str1, str2)	
+	fmt.Println(bool1, bool2)
 	
 }
