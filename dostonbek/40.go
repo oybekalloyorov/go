1ac9fac3479ebabed71bed0254c2ae27f6616121
oybekalloyorov <alloyorovoybek1997@gmail.com>
2025-10-21 13:36:02 +0500

    40 masala go worked

--- dostonbek/40.go
+++ dostonbek/40.go
@@ -2,23 +2,24 @@ package main
 
 import "fmt"
 
+func birlashtir(m, m2 map[string]int) map[string]int{
+	result := make(map[string]int)
+
+	for s, v := range m{
+		result[s] = v
+	}
+
+	for s2, v2 := range m2{
+		result[s2] += v2
+	}
+
+	return result
+}
+
 func main() {
 	mp := map[string]int{"a": 2, "b": 3}
-	mp2 := map[string]int{"b": 4, "c": 5}
+	mp2 := map[string]int{"b": 4, "c": 5, "b1": 4}
 
-	for s, w := range mp {
-		text := ""
-		son := 0
-		for s2, w2 := range mp2 {
-			if s == s2 {
-				mp[s] = w + w2
-			}
-			text = s2
-			son = w2
-		}
-		if son != 0 {
-			mp[text] = son 
-		}
-	}
-	fmt.Println(mp)
+	res := birlashtir(mp, mp2)
+	fmt.Println(res)
 }
