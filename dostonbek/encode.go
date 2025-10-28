6435783c18f5817978073823bd8793535dc52e51
oybekalloyorov <alloyorovoybek1997@gmail.com>
2025-10-28 16:13:50 +0500

    hex encode va decode qilish

--- dostonbek/encode.go
+++ dostonbek/encode.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"encoding/base64"
+	"encoding/hex"
 	"fmt"
 )
 
 func main() {
 	message := "hello,go (*w3hu%#"
 	demoBase64(message)
+	demoHex(message)
 }
+
 func demoBase64(message string) {
 	fmt.Println("--------Demo encoding base64--------")
 	fmt.Println("plaintext:")
@@ -18,5 +21,17 @@ func demoBase64(message string) {
 	fmt.Println(encoding)
 	decoding, _ := base64.StdEncoding.DecodeString(encoding)
 	fmt.Println("decoding base64 msg:")
+	fmt.Println(string(decoding))
+}
+
+func demoHex(message string) {
+	fmt.Println("--------Demo encoding Hex--------")
+	fmt.Println("plaintext:")
+	fmt.Println(message)
+	encoding := hex.EncodeToString([]byte(message))
+	fmt.Println("Hex msg:")
+	fmt.Println(encoding)
+	decoding, _ := hex.DecodeString(encoding)
+	fmt.Println("decoding Hex msg:")
 	fmt.Println(string(decoding))
 }
