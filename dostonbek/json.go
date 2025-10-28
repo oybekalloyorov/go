d4f3ddcb0bcea20e3b30f6d29572413d0df7ec97
oybekalloyorov <alloyorovoybek1997@gmail.com>
2025-10-28 16:53:49 +0500

    struct to json string

---
+++ dostonbek/json.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+func main() {
+	demoJson()
+}
+
+func demoJson() {
+	fmt.Println("--------Demo encoding json--------")
+	type Employee struct {
+		Id    string `json:"id"`
+		Name  string `json:"name"`
+		Email string `json:"email"`
+	}
+	// struct to json
+	fmt.Println(">>>>struct to json….")
+	emp := &Employee{Id: "12345", Name: "Michael", Email: "michael@email.com"}
+	b, err := json.Marshal(emp)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(b))
+	// json string to struct
+	fmt.Println(">>>>json to struct….")
+	var newEmp Employee
+	str := `{"Id":"4566","Name":"Brown","Email":"brown@email.com"}`
+	json.Unmarshal([]byte(str), &newEmp)
+	fmt.Printf("Id: %s\n", newEmp.Id)
+	fmt.Printf("Name: %s\n", newEmp.Name)
+	fmt.Printf("Email: %s\n", newEmp.Email)
+}
